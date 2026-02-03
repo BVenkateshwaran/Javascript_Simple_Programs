40293a16b016dab4fb35b3ae2db701f35a329621
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-02-03 22:32:22 +0530

    Updated checkPerfectNumber.js

--- Simple/checkPerfectNumber.js
+++ Simple/checkPerfectNumber.js
@@ -1,19 +1,34 @@
 let n = 28;
+// ! Check if the number is a Perfect Number or not.
+function isPerfect(n)
+{
+    if (n<=1)
+    {
+        return false;    
+    }
+    else
+    {
+        let sum = 0;
+        for(let i=1;i<n;i++)
+        {
+            if(n%i == 0)
+            {
+                sum = sum+i;
+            }
+        }
+        if(sum == n)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
 
-if (n <= 1) {
-    console.log(`${n} is not a perfect number.`);
-} 
+if (isPerfect(n)) {
+    console.log(`${n} is a perfect number.`);
+}
 else
 {
-    let sum = 0;
-    for (let i = 1; i <= n / 2; i++) {
-        if (n % i === 0) {
-            sum += i;
-        }
-    }
-    if (sum === n) {
-        console.log(`${n} is a perfect number.`);
-    } else {
-        console.log(`${n} is not a perfect number.`);
-    }
+    console.log(`${n} is not a perfect number.`);
 }
