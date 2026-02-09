348b112240aef4bb50d7ac88eeabd14b9c3ca2dc
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-02-09 19:28:33 +0530

    Programs Updated

--- Simple/printFibonacciSeries.js
+++ Simple/printFibonacciSeries.js
@@ -1,5 +1,9 @@
 let n = 15;
 
+// ! Fibonacci Series
+// ? 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
+// ^ The first two numbers of the Fibonacci series are 0 and 1 and each subsequent number is the sum of the previous two numbers. 
+
 let first = 1,second = 1;
 
 console.log(first);
