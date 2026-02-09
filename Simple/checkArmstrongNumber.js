e4a4911aa7b8c0f3b43bab5304a91b32c94d7499
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-02-09 19:27:20 +0530

    New Program Added : checkArmstrongNumber.js

---
+++ Simple/checkArmstrongNumber.js
@@ -0,0 +1,29 @@
+let a = 1634;
+
+
+let n = a.toString()
+let a1 = a;
+let l = n.length;
+console.log(`a = ${n} and length of ${n} = ${l}`);
+let c = 1,sum=0;
+while (a>0)
+{
+    let last = a%10;
+    c = 1;
+    for (let index = 1; index <= l; index++)
+    {
+        c = c * last;           
+    }
+    console.log(`Cube of ${last} = ${c}`);
+    sum = sum+c;
+    console.log(`Sum = ${sum}`);
+    a = Math.floor(a/10);
+}
+if(a1 == sum)
+{
+    console.log(`${a1} is a Armstrong Number`);
+}
+else
+{
+    console.log(`${a1} is not a Armstrong Number`);
+}
