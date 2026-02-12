1834c7e19d7cb6afcd0149a46432e36ad4713619
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-02-12 20:03:18 +0530

    Updated

--- Simple/celsiusToFahrenheit.js
+++ Simple/celsiusToFahrenheit.js
@@ -1,5 +1,9 @@
 let c = 36.5;
 
+// ! Celsius to Fahrenheit
+
+// ? The formula to convert Celsius to Fahrenheit is: F = (C * 9/5) + 32
+
 let f = (c * (9.0/5.0)) + 32;
 
 console.log(`${c} Celsius = ${f} Fahrenheit`);
